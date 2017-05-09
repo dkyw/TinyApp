7a0c324474d8ac9baf219e5a540071ffff089b14
Derrel Wong <derrellwong@gmail.com>
2017-05-09 19:10:24 +0000

    added routes for index and shortURL

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+
+app.get("/urls/:id", (req, res) => {
+  let longURL = urlDatabase[req.params.id];
+
+  console.log(longURL);
+  let templateVars = {
+    shortURL: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
+  res.render("urls_show", templateVars);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`TinyApp listening on port ${PORT}!`);
+});
